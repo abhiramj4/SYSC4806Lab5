8669b6d80a779310dcfb39015fc1d652ede24379
= <=>
2021-02-12 10:30:45 -0500

    up

---
+++ src/main/java/ThanosCorp/Lab4AbhiramSanthosh/HttpRequestTest.java
@@ -0,0 +1,17 @@
+package ThanosCorp.Lab4AbhiramSanthosh;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+}
